10bb24d542843bbcf0cab95d6b49f29cbca1bb10
Kauê Nascimento <kaue.sc78@gmail.com>
2025-06-22 16:32:49 -0300

    Projeto

---
+++ testes/teste1.py
@@ -0,0 +1,16 @@
+nota1 = input('Digite sua primeira nota: ')
+nota2 = input('Digite sua segunda nota: ')
+nota3 = input('Digite sua terceira nota: ')
+
+float_nota_1 = (float (nota1))
+float_nota_1 = (float (nota1))
+float_nota_2 = (float (nota2))
+
+resultado = (int_nota_1 + int_nota_2 + int_nota_3)
+
+if resultado > 7:
+    resultado = 10
+    print(f'Parabéns!!, Você antingiu os "{resultado}" pontos e foi aprovado.')
+
+else:
+    print(f'Infelizmente sua nota de "{resultado}" não foi suficente para você ser aprovado')
