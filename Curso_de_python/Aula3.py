6e2db25940fbc841f6d278945dcea4f614bfea88
Kauê Nascimento <kaue.sc78@gmail.com>
2025-06-24 22:30:28 -0300

    Aula3' git push

---
+++ Curso_de_python/Aula3.py
@@ -0,0 +1,26 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Kauê do Nascimento')
+print(1, 'Kauê "Nascimento"')
+
+# Aspas duplas
+print("kauê do Nascimento")
+print
+#     print('Sair')
+
+# Avaliação de curto circuito
+entrada = input (2, "Kauê 'Nascimento'")
+
+# Escape
+print("kauê \"Nasicmento\"")
+
+# r
+print(r"Kauê \"Nascimento\"")
