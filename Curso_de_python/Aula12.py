21d3caa404579fe7e3682149f70dd5afeb57c48b
Kauê Nascimento <kaue.sc78@gmail.com>
2025-07-03 23:12:52 -0300

    Aula 12

---
+++ Curso_de_python/Aula12.py
@@ -0,0 +1,8 @@
+#dividir o peso em quilogramas
+#pela altura em metros ao quadrado
+#(peso/(altura x altura))
+nome = 'kaue'
+altura = 1.75
+peso = 67
+imc = (peso / (altura*altura))
+print(f"{nome} tem {altura}m  de altura e pesa {peso}kg. Então seu imc é de {imc: .2f} ")
