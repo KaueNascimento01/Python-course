36b8f74ef9e188539e204fedc806200a3376dcbc
Kauê Nascimento <kaue.sc78@gmail.com>
2025-07-08 23:14:53 -0300

    Aula 19

---
+++ Curso_de_python/Aula19.py
@@ -0,0 +1,10 @@
+primeiro_valor = input('Digite valor: ')
+segundo_valor = input('Digite outro valor: ')
+if primeiro_valor > segundo_valor:
+    print(f'primeiro_valor "{primeiro_valor}" é maior que segundo_valor "{segundo_valor}"')
+
+elif primeiro_valor == segundo_valor:
+    print(f'primerio_valor "{primeiro_valor}" é igual a o segundo_valor "{segundo_valor}" ')
+
+else:
+    print(f'segundo_valor "{segundo_valor}" é maior que primeiro_valor "{primeiro_valor}"')
